80c4b003f6a93b4477e22f3591eaab92f1899586
RonKane2021 <85210547+RonKane2021@users.noreply.github.com>
2021-06-19 21:18:57 -0700

    Add files via upload

---
+++ cleaning files/searching for files with a word/2_Replacing words in text files.py
@@ -0,0 +1,29 @@
+#This program looks for specific word in text files and replaces them with any word or phrases.
+
+import os, re, shutil
+
+inpath = "C:\\test2\\"
+outpath= "C:\\test3\\"
+
+word = "hello"
+
+print ("Removing the following words that appear first: ", word, "\n")
+
+for filename in os.listdir(inpath): #this is a loop in python
+    filein = open(inpath + filename, 'r')
+    fileout = open(outpath + filename, 'w')
+    filedata = filein.read()
+    filein.close()
+    w1 = re.search(word, filedata, re.IGNORECASE)
+    if w1 == None:
+        fileout.close()
+    else:
+        new = filedata.replace(word, "world") #put new word here
+        fileout.write(new)
+        fileout.close()
+
+print ("Finish")
+
+    
+        
+
